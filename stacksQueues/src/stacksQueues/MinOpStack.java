651e53829b580cb5974895788fd18e64d38e21ba
Abhishek Bhadange <abhishekbhadange@gmail.com>
2014-10-26 16:00:40 -0700

    Initial Version

---
+++ stacksQueues/src/stacksQueues/MinOpStack.java
@@ -0,0 +1,29 @@
+// Min operation stack with push, pop and min operations taking O(1) time
+
+package stacksQueues;
+
+import java.util.Stack;
+
+public class MinOpStack extends Stack<Integer> {
+	public Stack<Integer> s2;
+	public MinOpStack() {
+		s2 = new Stack<Integer>();
+	}
+	public void push(int data) {
+		if(data <= min())
+			s2.push(data);
+		super.push(data);
+	}
+	public Integer pop() {
+		int value = super.pop();
+		if(value == min())
+			s2.pop();
+		return value;
+	}
+	public int min() {
+		if(s2.empty())
+			return Integer.MAX_VALUE;
+		else
+			return (Integer)s2.peek();
+	}
+} 
---
+++ stacksQueues/src/stacksQueues/MinOperation.java
@@ -0,0 +1,20 @@
+package stacksQueues;
+
+public class MinOperation {
+	public static void main(String[] args) {
+		MinOpStack theStack = new MinOpStack();
+		theStack.push(23);
+		theStack.push(12);
+		theStack.push(54);
+		theStack.push(5);
+		theStack.push(60);
+		int temp = theStack.pop();
+		System.out.println("Popped Item: " + temp);
+		int min = theStack.min();
+		System.out.println("Min Item: " + min);
+		temp = theStack.pop();
+		System.out.println("Popped Item: " + temp);
+		min = theStack.min();
+		System.out.println("Min Item: " + min);
+	}
+}	
